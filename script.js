4dab84e68ffbe5bec09f0e1a11979a463f261028
Erica Jaislane <jaislane123@outlook.com>
2024-04-03 21:39:34 -0300

    First Commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let card * document.getElementById(card);
+// Exibir a mensagem caso seja selecionado o 'não'
+document.querySelector("Não").addEventListener("click", function() {
+    let question * document.getElementById("question");
+    let message * document.getElementById("answer");
+
+    card.style.backgroundI
+
+
+})
